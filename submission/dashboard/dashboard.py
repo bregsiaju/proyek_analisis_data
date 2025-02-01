39f6543c234b5a841312b5146b7a64db7c9ab48c
Bregsi Atingsari Julastri <bregsiaju@gmail.com>
2025-02-01 10:27:14 +0700

    Update dashboard.py

--- submission/dashboard/dashboard.py
+++ submission/dashboard/dashboard.py
@@ -4,7 +4,9 @@ import seaborn as sns
 import streamlit as st
 sns.set_theme(style="dark")
 
-final_df = pd.read_csv("final_data.csv")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+csv_path = os.path.join(BASE_DIR, "final_data.csv")
+final_df = pd.read_csv(csv_path)
 
 datetime_columns = ["dteday"]
 final_df.sort_values(by="dteday", inplace=True)
@@ -37,7 +39,7 @@ weather_labels = {
 }
 
 with st.sidebar:
-    st.image("pedalgo.png")
+    st.image("./pedalgo.png")
     
     st.subheader("Filter Data")
     start_date, end_date = st.date_input(
